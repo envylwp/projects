2ee126c93e3c6e0993358952cfd7f65fc307e460
root <207239034@qq.com>
2018-03-09 09:42:25 +0800

    added:添加测试冲突

--- kafka/src/main/java/com/projects/Constant.java
+++ kafka/src/main/java/com/projects/Constant.java
@@ -7,4 +7,5 @@ public class Constant {
     public static final String KAFAK_ZK_SERVER = "192.168.18.101:2181,192.168.18.102:2181,192.168.18.103:2181";
     public static final String KAFAK_BROKER = "192.168.18.101:9092,192.168.18.102:9092,192.168.18.103:9092";
     public static final String KAFKA_CLUSTER_IP = "192.168.18.101,192.168.18.102,192.168.18.103";
+    public static final String TEST_88 = "test88";
 }
